8c9e0605e3a651ecef8accf1070c38704aca71cd
Akshay Raut <akshay.raut@gep.com>
2023-08-12 21:25:51 +0530

    attempt #1

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,14 +14,69 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('csharpclasstojson.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('csharpclasstojson.createJsonFromCSharp', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CSharpClassToJson!');
+		let editor = vscode.window.activeTextEditor;
+		if (editor) {
+			let text = editor.document.getText();
+			let finalJSON = generateSampleJSON(text);
+		}
+		vscode.window.showInformationMessage('Output will be visible in new tab!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+// Function to generate sample JSON from C# class
+// Function to generate sample JSON from C# class
+function generateSampleJSON(csharpClass: string): string {
+    // Parse the C# class structure
+    const classRegex = /class\s+(\w+)\s*{([^}]*)}/;
+    const match = csharpClass.match(classRegex);
+
+    if (!match) {
+        throw new Error('Invalid C# class structure');
+    }
+
+    const className = match[1];
+    const propertiesText = match[2].trim();
+
+    // Parse class properties
+    const propertyRegex = /\s*(\w+)\s+(\w+)\s*{.*?get;\s*set;\s*}/g;
+    const propertiesMatch = propertiesText.match(propertyRegex);
+
+    if (!propertiesMatch) {
+        throw new Error('No properties found in C# class');
+    }
+
+    const properties: { [key: string]: any } = {};
+    propertiesMatch.forEach(propertyMatch => {
+        const [, type, name] = propertyMatch.match(/\s*(\w+)\s+(\w+)/);
+        properties[name] = generateSampleValue(type);
+    });
+
+    // Generate JSON from properties
+    const sampleJSON = JSON.stringify({ [className]: properties }, null, 4);
+    return sampleJSON;
+}
+
+// Function to generate sample value based on C# type
+function generateSampleValue(type: string): any {
+    switch (type) {
+        case 'string':
+            return 'Sample String';
+        case 'int':
+        case 'double':
+        case 'float':
+        case 'decimal':
+            return 0;
+        case 'bool':
+            return false;
+        default:
+            return null;
+    }
+}
