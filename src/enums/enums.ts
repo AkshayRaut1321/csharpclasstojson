894f171019d03a64e62d6c961a6f63ed70dfbfd7
Akshay Raut <rakshay08@gmail.com>
2023-08-20 20:21:22 +0530

    ignore fields match

---
+++ src/enums/enums.ts
@@ -0,0 +1,9 @@
+export enum NamingConvention {
+    camelCase = "camelCase",
+    PascalCase = "PascalCase"
+}
+
+export enum SupportFields {
+    ignoreFields = "ignoreFields",
+    includeFields = "includeFields"
+}
